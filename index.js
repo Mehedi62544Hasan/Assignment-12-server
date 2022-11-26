61c4661e22069bab1bdb7a8a58a70d564542ac38
Mehedi62544Hasane <mehedi62544hasan@gmail.comm>
2022-11-26 17:01:22 +0000

    modal create

--- index.js
+++ index.js
@@ -42,14 +42,15 @@ async function run() {
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
-            const query = {booking} 
+            
+            // const query = {booking} 
 
-            const alreadyBooked = await bookingsCollection.find(query).toArray();
+            // const alreadyBooked = await bookingsCollection.find(query).toArray();
 
-            if (alreadyBooked.length) {
-                const message = `You already have a booking on ${booking.appointmentDate}`
-                return res.send({ acknowledged: false, message })
-            }
+            // if (alreadyBooked.length) {
+            //     const message = `You already have a booking on ${booking.appointmentDate}`
+            //     return res.send({ acknowledged: false, message })
+            // }
 
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
